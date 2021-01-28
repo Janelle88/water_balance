2826d5136b48fdc96e2ce9c8071c6479049ce87c
Janelle88 <jnchristensen1788@gmail.com>
2021-01-28 15:54:21 -0800

    updated run_rmd to encompass both data download and graph creation; updated model selection information

--- run_rmd.R
+++ run_rmd.R
@@ -7,7 +7,7 @@ library(here)
 # for each type of car in the data create a report
 # these reports are saved in output_dir with the name specified by output_file
 
-centroids <- read_csv(here::here("pisp.csv")) #NCPN_centroids
+centroids <- read_csv(here::here("NCPN_centroids.csv")) #NCPN_centroids
 
 for(row in 1:nrow(centroids)) {
   sites <- centroids[row, "Park"]
@@ -35,15 +35,37 @@ for(row in 1:nrow(centroids)) {
   
   wet_year <-  2016 # particularly wet year for people to reference
   
-  # if(file.exists(here::here(site, paste("water_balance_graphs_for_", site, ".html", sep="")))){
-  #   next
-  # }
+  if(file.exists(here::here(site, paste("water_balance_data_for_", site, ".html", sep="")))){
+    next
+  }
   
-  rmarkdown::render(here::here("water_balance_graphs.Rmd"),  # file 2
-                    output_file =  paste("water_balance_graphs_for_", site, ".html", sep=""), 
+  rmarkdown::render(here::here("water_balance_data.Rmd"),  # file 2
+                    output_file =  paste("water_balance_data_for_", site, ".html", sep=""), 
                     output_dir = here::here("sites", site))
   
+} #close data download loop
+
+for(row in 1:nrow(centroids)) {
+  sites <- centroids[row, "Park"]
+  lats <- centroids[row, "Lat"]
+  lons <- centroids[row, "Long"]
+  model_bcs <- centroids[row, "model_bc"]
+  model_bc_rcps <- centroids[row, "model_bc_rcp"]
+  model_wcs <- centroids[row, "model_wc"]
+  model_wc_rcps <- centroids[row, "model_wc_rcp"]
+  
+  
+  site <- pull(sites, Park)
+  lat <- pull(lats, Lat)
+  lon <- pull(lons, Long)
+  model_bc <- pull(model_bcs, model_bc)
+  model_bc_rcp <- pull(model_bc_rcps, model_bc_rcp)
+  model_wc <- pull(model_wcs, model_wc)
+  model_wc_rcp <- pull(model_wc_rcps, model_wc_rcp)
+  past_data <-  "gridMET"
+  
+rmarkdown::render(here::here("water_balance_graphs.Rmd"),  # file 2
+                  output_file =  paste("water_balance_graphs_for_", site, ".html", sep=""), 
+                  output_dir = here::here("sites", site))
 }
-
-
   
